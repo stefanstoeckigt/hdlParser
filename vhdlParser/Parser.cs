460fd32538c85b8c47f4a940e0bbf87b41b5ee58
stefan stoeckigt <s.stoeckigt@gmail.com>
2019-01-13 15:33:45 +0000

    Update: Added the start to Architecture Parser.

--- vhdlParser/Parser.cs
+++ vhdlParser/Parser.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using System;
+using vhdl;
 
 // How-To: https://tomassetti.me/getting-started-with-antlr-in-csharp/
-namespace vhdlParser
+namespace VHDL.Parser
 {
     public class Parser
     {
+        public Parser (string data)
+        {
+            try
+            {
+
+                AntlrInputStream inputStream = new AntlrInputStream(data);
+                var lexer = new vhdlLexer(inputStream);
+                var commonTokenStream = new CommonTokenStream(lexer);
+                var parser = new vhdlParser(commonTokenStream);
+
+                var architectureParser = new parser.ArchitectureParser(parser.architecture_body());
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+
     }
 }
---
+++ vhdlParser/parser/ArchitectureParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using vhdl;
+
+namespace VHDL.Parser.parser
+{
+    class ArchitectureParser
+    {
+        /// <summary>
+        /// architecture_body
+        /// : ARCHITECTURE identifier OF identifier IS
+        /// architecture_declarative_part
+        /// BEGIN
+        /// architecture_statement_part
+        /// END ( ARCHITECTURE )? ( identifier )? SEMI
+        /// ;
+        /// </summary>
+        /// <param name="ctx"></param>
+        public ArchitectureParser(vhdlParser.Architecture_bodyContext ctx)
+        {
+        }
+    }
+}
